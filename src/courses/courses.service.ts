a8a96805258be0f351acf32d1e2d6648d7639089
Jaime Vargas <jaime.osorno@rocketfy.co>
2025-05-21 14:18:15 -0500

    feat(database): implementar conexión MongoDB y modelo de cursos
    - Configurar conexión a MongoDB en app.module.ts
    - Implementar modelo/schema para cursos
    - Crear funcionalidad para insertar cursos usando el modelo

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Course } from './model/course.schema';
+import { Model } from 'mongoose';
+import { User } from 'src/users/model/user.schema';
 
 @Injectable()
 export class CoursesService {
+  constructor(
+    @InjectModel(Course.name) private readonly CourseModel: Model<Course>,
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+
   create(createCourseDto: CreateCourseDto) {
-    return 'This action adds a new course';
+    return this.CourseModel.create(createCourseDto);
   }
 
   findAll() {
@@ -17,6 +26,7 @@ export class CoursesService {
   }
 
   update(id: number, updateCourseDto: UpdateCourseDto) {
+    console.log(updateCourseDto);
     return `This action updates a #${id} course`;
   }
 
